aa411bc832b9e8295ba30f6b9ca25de1d35b651a
OlgaVino25 <olgavinokyrova25@yandex.ru>
2025-10-15 12:19:12 +0300

    replacing "\" in the address

--- render_website.py
+++ render_website.py
@@ -15,6 +15,10 @@ meta_data_path = os.path.join(script_dir, "meta_data.json")
 with open(meta_data_path, "r", encoding="utf-8") as my_file:
     books = json.load(my_file)
 
+for book in books:
+    if "img_src" in book:
+        book["img_src"] = book["img_src"].replace("\\", "/")
+
 books_sorted = sorted(books, key=lambda x: x["title"])
 
 rendered_page = template.render(books=books_sorted)
