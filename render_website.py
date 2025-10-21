15e10081de0655cf12750c10b1207d88c8297fb6
OlgaVino25 <olgavinokyrova25@yandex.ru>
2025-10-21 10:17:47 +0300

    Integrate livereload

--- render_website.py
+++ render_website.py
@@ -3,22 +3,27 @@ import os
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 
-env = Environment(
-    loader=FileSystemLoader("."), autoescape=select_autoescape(["html", "xml"])
-)
+def render_website():
+    env = Environment(
+        loader=FileSystemLoader("."), autoescape=select_autoescape(["html", "xml"])
+    )
 
-template = env.get_template("template.html")
+    template = env.get_template("template.html")
 
-script_dir = os.path.dirname(os.path.abspath(__file__))
-meta_data_path = os.path.join(script_dir, "meta_data.json")
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    meta_data_path = os.path.join(script_dir, "meta_data.json")
 
-with open(meta_data_path, "r", encoding="utf-8") as my_file:
-    books = json.load(my_file)
+    with open(meta_data_path, "r", encoding="utf-8") as my_file:
+        books = json.load(my_file)
 
-for book in books:
-    book["img_src"] = book["img_src"].replace("\\", "/")
+    for book in books:
+        book["img_src"] = book["img_src"].replace("\\", "/")
 
-rendered_page = template.render(books=books)
+    rendered_page = template.render(books=books)
 
-with open("index.html", "w", encoding="utf8") as file:
-    file.write(rendered_page)
+    with open("index.html", "w", encoding="utf8") as file:
+        file.write(rendered_page)
+
+
+if __name__ == "__main__":
+    render_website()
